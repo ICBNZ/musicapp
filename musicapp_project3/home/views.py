8abb54a2ef70faceb45d94732dffc46f581eb3e3
IslaBecker91031015 <islabecker@gmail.com>
2019-02-25 02:28:46 +1300

    Liam Green sign up form/Js refactoring
    Sign up registration form added to login page, styling login/ sign up forms and javascript refactored

--- musicapp_project3/home/views.py
+++ musicapp_project3/home/views.py
@@ -17,6 +17,21 @@ def home(request):
     students = Student.objects
     return render(request, 'home.html', {'tutors': tutors},  {'students': students})
 
+#### sign up form
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
 #### Logged In User Profile
 @login_required(login_url="/accounts/login/")
 def profile(request, id):
